3056a2d6bbb8b20e353e7289c3eb4a1165c54204
raph <raph@hpar.fr>
2015-09-08 15:42:23 +0200

    add AccessError exception

--- src/components/odoo/jsonRpc-service.js
+++ src/components/odoo/jsonRpc-service.js
@@ -238,6 +238,9 @@ angular.module('odoo').provider('jsonRpc', function jsonRpcProvider() {
 				} else if ( (error.message === "Odoo Server Error" && /FATAL:  database "(.+)" does not exist/.test(error.data.message))) {
 					errorObj.title = "database_not_found";
 					errorObj.message = error.data.message;
+				} else if ( (error.data.name === "openerp.exceptions.AccessError")) {
+					errorObj.title = 'AccessError';
+					errorObj.message = error.data.message;
 				} else {
 					var split = ("" + error.data.fault_code).split('\n')[0].split(' -- ');
 					if (split.length > 1) {
